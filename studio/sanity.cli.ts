5aa24e38a902b3e453136e27f734b98c63e43bd6
Kevin Ku <kku1993dev@gmail.com>
2026-08-18 11:19:50 -0700

    Add appId to sanity deployment config

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -11,6 +11,7 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'kf2hpa96alo5ap3plotla591',
   },
   typegen: {
     enabled: true,
